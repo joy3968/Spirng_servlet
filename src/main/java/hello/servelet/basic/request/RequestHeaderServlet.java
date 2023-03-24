4ca61834a74d164ca087a5584ea58767e8d95e42
minwoo <sasyo1z2@naver.com>
2023-03-25 00:48:39 +0900

    header 조회

--- src/main/java/hello/servelet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servelet/basic/request/RequestHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servelet.basic.request;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,6 +16,7 @@ public class RequestHeaderServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         printStartLine(request);
         printHeaders(request);
+        printHeaderUtils(request);
     }
 
     private static void printStartLine(HttpServletRequest request) {
@@ -50,6 +52,42 @@ public class RequestHeaderServlet extends HttpServlet {
                         .forEachRemaining(headerName -> System.out.println("headerName = " + headerName));
 
         System.out.println("--- Headers - end ---");
+        System.out.println();
+    }
+
+    //Header 편리한 조회
+    private void printHeaderUtils(HttpServletRequest request) {
+        System.out.println("--- Header 편의 조회 start ---");
+        System.out.println("[Host 편의 조회]");
+        System.out.println("request.getServerName() = " +
+            request.getServerName()); //Host 헤더
+        System.out.println("request.getServerPort() = " +
+            request.getServerPort()); //Host 헤더
+        System.out.println();
+
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " + locale));
+        System.out.println("request.getLocale() = " + request.getLocale());
+        System.out.println();
+
+        System.out.println("[cookie 편의 조회]");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }
+        }
+        System.out.println();
+
+        System.out.println("[Content 편의 조회]");
+        System.out.println("request.getContentType() = " +
+                request.getContentType());
+
+        System.out.println("request.getContentLength() = " +request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " +
+                request.getCharacterEncoding());
+
+        System.out.println("--- Header 편의 조회 end ---");
         System.out.println();
     }
 }
