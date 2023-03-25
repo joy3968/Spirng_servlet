84d8078e488549880d7be48447a3ac59924456a6
minwoo <sasyo1z2@naver.com>
2023-03-25 23:52:43 +0900

    HTTP 요청데이터 - API 메세 바디 - JSON

---
+++ src/main/java/hello/servelet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.servelet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
